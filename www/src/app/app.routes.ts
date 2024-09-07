e4004c92aef47042fe97eb42f367ca3abab5ff11
alfayedtch <albos7484@gmail.com>
2024-09-07 16:23:49 +0200

    sideMenu

--- www/src/app/app.routes.ts
+++ www/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AccountComponent } from './pages/account/account.component';
+import { SideComponent } from './layout/side/side.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'account',
+    component: AccountComponent
+  },
+  {
+    path:'side',
+    component: SideComponent,
+    children:[
+      {
+        path: 'login',
+        component:LoginComponent
+      }
+    ]
+  }
+];
---
+++ www/src/app/layout/side/side.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-side',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './side.component.html',
+  styleUrl: './side.component.css'
+})
+export class SideComponent {
+
+}
