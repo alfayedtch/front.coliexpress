1ed81ac643ab231ba5dcad747f19b9e48ab48457
alfayedtch <albos7484@gmail.com>
2025-03-20 11:41:05 +0100

    liste des roles

--- www/src/app/interfaces/role.ts
+++ www/src/app/interfaces/role.ts
@@ -1,4 +1,5 @@
 export interface Role {
+  id?: number
   libelle?: string;
   code?: string;
 }
--- www/src/app/pages/admin/role/role-list/role-list.component.ts
+++ www/src/app/pages/admin/role/role-list/role-list.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { Role } from '../../../../interfaces/role';
+import { RoleService } from '../../../../services/role/role.service';
+import { MessageService, SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-role-list',
   standalone: true,
-  imports: [],
+  imports: [TableModule, TagModule, CommonModule, TagModule, DropdownModule, ButtonModule, InputTextModule,FormsModule],
   templateUrl: './role-list.component.html',
-  styleUrl: './role-list.component.css'
+  styleUrl: './role-list.component.css',
+  providers: [MessageService]
 })
-export class RoleListComponent {
+export class RoleListComponent implements OnInit {
+  roles: Role[] =[];
+  selectedRoles: Role[] =[];
+  statuses!: SelectItem[];
 
+  clonedRoles: { [s: string]: Role } = {};
+
+  constructor(
+    private roleService: RoleService,
+    private messageService: MessageService
+  ){
+
+  }
+
+
+  ngOnInit(): void {
+    this.initRoles();
+  }
+
+  initRoles(){
+    this.roleService.getRoles().subscribe(
+      (response:any) => {this.roles = <Role[]>response.roles}
+    )
+  }
+
+  onRowEditInit(role: Role) {
+    this.clonedRoles[role.id as unknown as string] = { ...role };
+}
+
+onRowEditSave(role: Role) {
+    if (role.code) {
+        delete this.clonedRoles[role.id as unknown as string];
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Role is updated' });
+    } else {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Invalid Price' });
+    }
+}
+
+onRowEditCancel(role: Role, index: number) {
+    this.roles[index] = this.clonedRoles[role.id as unknown as string];
+    delete this.clonedRoles[role.id as unknown as string];
+}
 }
