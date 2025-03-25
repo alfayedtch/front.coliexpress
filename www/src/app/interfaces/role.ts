94c7238cfd33a54d82ceac0fa34469a95b357cfe
alfayedtch <albos7484@gmail.com>
2025-03-25 20:03:16 +0100

    refacto

--- www/src/app/interfaces/role.ts
+++ www/src/app/interfaces/role.ts
@@ -1,5 +1,6 @@
 export interface Role {
   id?: number
+  company_id?: number
   libelle?: string;
   code?: string;
 }
--- www/src/app/pages/admin/user/user-create/user-create.component.ts
+++ www/src/app/pages/admin/user/user-create/user-create.component.ts
@@ -6,6 +6,9 @@ import { CommonModule } from '@angular/common';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { RoleService } from '../../../../services/role/role.service';
+import { CompanyService } from '../../../../services/company/company.service';
+import { Role } from '../../../../interfaces/role';
 
 @Component({
   selector: 'app-user-create',
@@ -19,8 +22,15 @@ import { MessageService } from 'primeng/api';
 export class UserCreateComponent {
 loading = false;
   userForm!: FormGroup;
+
+  rolesList: any[] = []; // Liste des privilèges depuis l'API
+  companiesList: any[] = []; // Liste des privilèges depuis l'API
+
+
 constructor(
   private userService: UserService,
+  private roleService: RoleService,
+  private companyService: CompanyService,
   private router: Router,
   private messageService: MessageService
 
@@ -30,12 +40,27 @@ constructor(
 
   ngOnInit(): void {
     this.initUserForm();
+    this.loadcompanies();
   }
 
   initUserForm(){
     this.userForm = new FormGroup(
       {
-        email: new FormControl('', [Validators.required,Validators.email]),
+        email: new FormControl((this.userForm?.controls['email'].value)?this.userForm.controls['email'].value:'', [Validators.required,Validators.email]),
+        isAdmin: new FormControl((this.userForm?.controls['isAdmin'].value)?this.userForm.controls['isAdmin'].value:'', [Validators.required]),
+        company_id: new FormControl((this.userForm?.controls['company_id'].value)?this.userForm.controls['company_id'].value:'', [Validators.required]),
+      }
+    )
+  }
+
+
+  initAdminUserForm(){
+    this.userForm = new FormGroup(
+      {
+        email: new FormControl(this.userForm.controls['email'].value, [Validators.required,Validators.email]),
+        isAdmin: new FormControl(this.userForm.controls['isAdmin'].value, [Validators.required]),
+        company_id: new FormControl(this.userForm?.controls['company_id'].value, [Validators.required]),
+        role_id: new FormControl('', [Validators.required]),
       }
     )
   }
@@ -62,6 +87,23 @@ constructor(
         this.loading = false;
         console.log('Création terminée');
       }
+    });
+  }
+
+  // Charger les roles depuis l'API
+  loadRoles(event: Event) {
+    const id: number = Number((event.target as HTMLSelectElement).value);
+
+    this.roleService.getRoles().subscribe((response: any) => {
+      this.rolesList = response.roles.filter((role: Role) => role.company_id === id);
+    });
+  }
+
+
+  // Charger les compagnies depuis l'API
+  loadcompanies() {
+    this.companyService.getCompanies().subscribe((response:any) => {
+      this.companiesList = response.companies;
     });
   }
 
